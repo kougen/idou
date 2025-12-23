023bf53f480a2a482cf995985e253f6ee3920aa3
Joshua Hegedus <josh.hegedus@outlook.com>
2025-12-24 00:29:58 +0100

    more comment fixes

--- idou.Core/Domain/EntityKey.cs
+++ idou.Core/Domain/EntityKey.cs
@@ -3,4 +3,10 @@ namespace idou.Core.Domain;
 public class EntityKey(string value)
 {
     public string Value { get; } = value;
+
+    public override string ToString() => Value;
+
+    public override int GetHashCode() => Value.GetHashCode();
+
+    public override bool Equals(object? obj) => obj is EntityKey key && Value == key.Value;
 }
--- idou.Core/Domain/EntityType.cs
+++ idou.Core/Domain/EntityType.cs
@@ -3,4 +3,10 @@ namespace idou.Core.Domain;
 public class EntityType(string name)
 {
     public string Name { get; } = name;
+
+    public override string ToString() => Name;
+
+    public override int GetHashCode() => Name.GetHashCode();
+
+    public override bool Equals(object? obj) => obj is EntityType type && Name == type.Name;
 }
